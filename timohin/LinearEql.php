430112361005ed161838a3c50f9d98b09cc08db6
429272-26 <429272-26>
2021-03-16 11:02:26 +0300

    lib files

---
+++ timohin/LinearEql.php
@@ -0,0 +1,18 @@
+<?php namespace timohin;
+class LinearEql{
+    protected $x;
+    public function solve($a, $b){
+        if($a==0 && $b!=0){
+            throw new TimohinExeption('Error: eq not exist');
+        }
+        MyLog::log('Eq is linear');
+        if($a==$b && $a==0){
+            $this->x=[INF];
+        }
+        if($a!=0){
+            $this->x=[-$b/$a];
+        }
+        return $this->x;
+    }
+}
+?>
